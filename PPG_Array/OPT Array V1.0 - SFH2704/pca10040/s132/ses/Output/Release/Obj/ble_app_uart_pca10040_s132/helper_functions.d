50361c998cf3fec1a695414e3bee0f1ed1b31da1
Eric Wang <ericrwang2011@gmail.com>
2026-03-18 14:33:30 -0500

    adjusted project for pd

---
+++ PPG_Array/OPT Array V1.0 - SFH2704/pca10040/s132/ses/Output/Release/Obj/ble_app_uart_pca10040_s132/helper_functions.d
@@ -0,0 +1,3 @@
+Output/Release/Obj/ble_app_uart_pca10040_s132/helper_functions.o: \
+ C\:\Users\philt\Box\Lu\ Lab\nRF\ Code\Nordic\ SDK\ 15.3\projects\PPG\MAX86141\my_drivers\src\helper_functions.c \
+ ../../../my_drivers/include/helper_functions.h
---
+++ PPG_Array/OPT Array V1.0 - SFH2704/pca10056e/s112/ses/Output/ble_app_uart_pca10056e_s112 Release/Obj/thumb_crt0.d
@@ -0,0 +1,2 @@
+Output/ble_app_uart_pca10056e_s112\ Release/Obj/thumb_crt0.o: \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.22/source/thumb_crt0.s
